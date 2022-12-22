66bf50af0a2fe57e1714c1c054f2f54627ac2e2b
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2022-12-21 23:02:17 -0300

    Aprendendo a como salvar informações

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,52 @@ let criaLi = () =>{
     return li;
 }
 
+let limpaInput=()=>{
+    inputTarefa.value = "";
+    inputTarefa.focus();
+}
+
+let criaBotaoApagar = (li) =>{
+    li.innerText += ' ';
+    const botaoApagar = document.createElement("button");
+    botaoApagar.innerText = "Apagar";
+    botaoApagar.setAttribute("class", "apagar");
+    li.appendChild(botaoApagar);
+}
+
+let criaTarefa = (textoInput)=>{
+    const li = criaLi();
+    li.innerHTML = textoInput;
+    tarefas.appendChild(li);
+    limpaInput();
+    criaBotaoApagar(li);
+    salvarTarefas();
+}
+
+let salvarTarefas = () =>{
+    const liTarefas = tarefas.querySelectorAll('li');
+    const listaDeTarefas = [];
+
+    for(let tarefa of liTarefas){
+        let tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace("Apagar", "").trim();
+        listaDeTarefas.push(tarefaTexto);
+    }
+
+    const tarefasJSON = JSON.stringify(listaDeTarefas);
+    localStorage.setItem('tarefas', tarefasJSON);
+}
+
+let adicionaTarefasSalvas = ()=>{
+    const tarefas = localStorage.getItem('tarefas');
+    const listaDeTarefas = JSON.parse(tarefas);
+    
+    for(let tarefa of listaDeTarefas){
+        criaTarefa(tarefa);
+    }
+}
+adicionaTarefasSalvas();
+
 inputTarefa.addEventListener("keypress", (e)=>{
     if(e.keyCode === 13){
         if(!inputTarefa.value) return;
@@ -15,17 +61,16 @@ inputTarefa.addEventListener("keypress", (e)=>{
     }
 })
 
-let criaTarefa = (textoInput)=>{
-    const li = criaLi();
-    li.innerHTML = textoInput;
-    tarefas.appendChild(li);
-}
-
 document.addEventListener('click', (e)=>{
     el = e.target;
     
     if(el.classList.contains("btn-tarefa")){
         if(!inputTarefa.value) return;
         criaTarefa(inputTarefa.value);
+    }
+
+    if(el.classList.contains("apagar")){
+        el.parentElement.remove();
+        salvarTarefas();
     }
 })
